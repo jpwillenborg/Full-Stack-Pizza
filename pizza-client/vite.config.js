600d63b876ce320d2595e03d8be8730711568705
JP Willenborg <jpwillenborg@users.noreply.github.com>
2026-09-11 09:39:14 -0500

    Update

--- pizza-client/vite.config.js
+++ pizza-client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5175, // Replace with your preferred port number
+  },
   
   // 📍 CRITICAL WHITE PAGE FIX: Forces all asset paths to compile relatively
   base: './'
